4240da8adeee16b47608d4832a2da7e4da328257
AlejoFerrari463 <alejoferrari463@gmail.com>
2024-04-29 00:35:43 -0300

    mostrar el pokemon por id o nombre terminado

--- javascript/nombre.js
+++ javascript/nombre.js
@@ -8,27 +8,57 @@ const main = document.querySelector("#mainNombre")
 
 const mostrarContenedor = document.createElement("div")
 mostrarContenedor.classList.add("mostrar-contenedor");
+mostrarContenedor.classList.add("card");
 
 
 
 mostrarContenedor.innerHTML=`
+
+    <p class="mostrar-id" >${datos[0].id}</p>
+
+    
+
     <h1>Nombre: ${datos[0].nombre}</h1>
-    <h2>Poke-ID: ${datos[0].id}</h2>
-    <img class="card-img-top" src=${datos[0].imagen} alt="">
-    <h3>Peso: ${((datos[0].peso)/10)}kg</h3>
-    <h3>Altura: ${((datos[0].altura)*10)}cm</h3>
-    <h4>Movimientos: ${datos[0].movimientos}</h4>
+            
+    <img class="card-img-top ms-2" src=${datos[0].imagen} alt="">
+    
+    <h3 class="mb-5" >Peso: ${((datos[0].peso)/10)}kg | Altura: ${((datos[0].altura)*10)}cm</h3>
+    
+    
+    
+    <h3 class="mb-5" >Movimientos: ${datos[0].movimientos}</h3>
+
+    
+
+    
+
+    
+
 
 
 `
 
+const mostrarHabilidades = document.createElement("div");
+mostrarHabilidades.classList.add("mostrar-habilidades");
+
+let habilidad = 1;
+
 habilidades.forEach((element) => {
     
     const h4 = document.createElement("h4")
-    h4.innerText= `${element}`
+    h4.innerHTML= `
+    ${element}
+    <div class="fs-6 mt-2" >
+        (hability ${habilidad})
+    </div>
+    
+    `
 
-    mostrarContenedor.appendChild(h4)
+    mostrarHabilidades.appendChild(h4)
 
+    habilidad++
 });
+
+mostrarContenedor.appendChild(mostrarHabilidades)
 
 main.appendChild(mostrarContenedor);
